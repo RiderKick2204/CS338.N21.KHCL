54ebf02c62aaa01348dd129430c59f0fd102f628
RiderKick2204 <100670190+RiderKick2204@users.noreply.github.com>
2023-07-10 13:02:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ with container:
             y = " ".join(str(x) for x in y_class)
             y = int(y)
             res = labels[y]
-            S = ['Predict label' + res]
+            S = ['Predicted: ' + res]
             # for i in range(len(answer[0])):
             #     S.append(labels[i] + ': ' + str("{0:.2%}".format(answer[0][i])))
             if (label):
